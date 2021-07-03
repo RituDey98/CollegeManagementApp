e9dc3e13263028df2b15441615e30934015abb6c
NairanjanaSarkar <sarkaemayuri98@gmail.com>
2021-07-04 00:40:36 +0530

    sign in and sign up page for both teacher and student added
    have not link them with any one...
    just added the pages...

---
+++ DepartmentManagement/app/src/main/java/com/ritudey/departmentmanagement/Activities/SignupTeacherActivity.java
@@ -0,0 +1,16 @@
+package com.ritudey.departmentmanagement.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ritudey.departmentmanagement.R;
+
+public class SignupTeacherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_teacher);
+    }
+}
